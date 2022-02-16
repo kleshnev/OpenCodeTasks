4c1b7b9460f6f91d8a3416d12af03ddc7ef50161
kleshnev <kleshnevden@mail.ru>
2022-02-16 10:55:28 +0400

    Added IllegalArgumentException

--- Solutions/src/stage1/TasksPack2/Task3/Task3.java
+++ Solutions/src/stage1/TasksPack2/Task3/Task3.java
@@ -1,8 +1,6 @@
 package stage1.TasksPack2.Task3;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Task3 {
@@ -12,13 +10,15 @@ public class Task3 {
         String inputStr = in.nextLine();
         String[] strArray = inputStr.split(" ");
         System.out.println("Введите номер позиции символа для замены");
+
         int position = in.nextInt();
+        if (position < 0) throw new IllegalArgumentException("Введите правильный номер символа");
+
         System.out.println("Введите символ для замены");
         char symbolReplacement = in.next().charAt(0);
         ArrayList<String> resultArray = getReplacedWords(strArray, position, symbolReplacement);
 
-        for (String word :
-                resultArray) {
+        for (String word : resultArray) {
             System.out.print(word + " ");
         }
     }
@@ -27,9 +27,8 @@ public class Task3 {
 
         StringBuilder builder = new StringBuilder();
         ArrayList<String> resultArray = new ArrayList<>();
-        for (String word :
-                strArray) {
-            if (position <= word.length()) {
+        for (String word : strArray) {
+            if (position < word.length()) {
                 builder.append(word);
                 builder.setCharAt(position, symbolReplacement);
                 resultArray.add(builder.toString());
