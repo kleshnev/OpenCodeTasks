6aaea3b00928c7e43865d017a105a4ecd4e60ddb
kleshnev <kleshnevden@mail.ru>
2022-02-18 12:58:00 +0400

    Fixed getFibonacciSequence method

--- Solutions/src/stage1/TasksPack2/Task2/Task2.java
+++ Solutions/src/stage1/TasksPack2/Task2/Task2.java
@@ -6,7 +6,7 @@ public class Task2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
-        String formatted = str.replaceAll("[^a-zA-Zа-яА-Я\\s]","");
+        String formatted = str.replaceAll("[^a-zA-Zа-яА-ЯёЁ\\s]","");
         System.out.println(formatted);
     }
 }
